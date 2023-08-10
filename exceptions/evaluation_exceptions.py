c517f3ac23933f3c0db702261c3cbc3e667bafeb
ndossche <niels.dossche@ugent.be>
2023-08-10 15:44:35 +0200

    cleanup

--- exceptions/evaluation_exceptions.py
+++ exceptions/evaluation_exceptions.py
@@ -5,18 +5,21 @@ from dodona.dodona_command import ErrorType
 
 class ValidationError(FeedbackException):
     """Base class for evaluation related exceptions in this module."""
+
     def __init__(self, trans: Translator, msg: str, line: int, pos: int):
         super(ValidationError, self).__init__(trans=trans, msg=msg, line=line, pos=pos)
 
 
 class LocatableValidationError(ValidationError):
     """Exceptions that can be located"""
+
     def __init__(self, trans: Translator, msg: str, line: int, pos: int):
         super(LocatableValidationError, self).__init__(trans=trans, msg=msg, line=line, pos=pos)
 
+
 class TestRuntimeError(ValidationError):
     """Exception that indicates that the test had a runtime error, e.g. crashed"""
+
     def __init__(self, trans: Translator, line: int, pos: int):
         msg = trans.human_error(ErrorType.RUNTIME_ERROR)
         super(TestRuntimeError, self).__init__(trans=trans, msg=msg, line=line, pos=pos)
-
